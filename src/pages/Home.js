12bc5477fd2108eda53b451d9cf14eec6c360afb
abonnet <anthony.bonnet1@orange.fr>
2022-11-10 17:05:34 +0100

    Ajout bouton lancement de quizz

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,13 +10,7 @@ export default function Home() {
   }, []);
   return (
     <>
-    <div className="card border-primary mb-3">
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Primary card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
+      <button className="btn-lancer-quizz">Lancer un Quizz !</button>
     </>
   )
 }
